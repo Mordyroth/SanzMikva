05fc3bd2e17ca76e71214fdc1419f4f5962a591f
pm3@estatic-infotech.in <Estatic123$#>
2019-07-29 16:55:33 +0530

    commit

--- app/src/main/java/com/example/android/miveh2/utils/PreferenceUtils.java
+++ app/src/main/java/com/example/android/miveh2/utils/PreferenceUtils.java
@@ -46,6 +46,7 @@ public class PreferenceUtils {
         if (sharedPreferences != null) {
             return sharedPreferences.getBoolean(key, false);
         }
+        
         return false;
     }
 
